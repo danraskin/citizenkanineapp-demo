46e948db47d5f99c0505856acf00fb877cb125e6
Dan Raskin <Danieltraskin@gmail.com>
2022-11-29 14:47:10 -0600

    small edit to congig

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -19,7 +19,7 @@ if (process.env.DB_PASS) {
   config = {
     user: 'citizenkanine',
     host: 'db.bit.io',
-    database: 'DANRASKIN/citizen_kanine',
+    database: 'danraskin/citizen_kanine',
     password: process.env.DB_PASS, // key from bit.io database page connect menu
     port: 5432,
     ssl: true,
