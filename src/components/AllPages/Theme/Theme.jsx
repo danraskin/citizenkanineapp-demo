4d7325899b8b8a9c8d1379a3d4b255dcaeda2ded
Dan Raskin <Danieltraskin@gmail.com>
2023-02-02 13:34:19 -0600

    updated  breakpoints to theme

--- src/components/AllPages/Theme/Theme.jsx
+++ src/components/AllPages/Theme/Theme.jsx
@@ -51,5 +51,13 @@ export const theme = createTheme({
       },
   },
 
-
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 550,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  }
 });
